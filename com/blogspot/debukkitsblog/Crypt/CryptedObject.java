5335fc6cdbc82a2abba12d04c9d6014b74af6d1c
Leonard <DeBukkIt@users.noreply.github.com>
2017-04-01 19:53:31 +0200

    Added the CryptedObject class
    This class contains AES encrypted bytes. That's all.

---
+++ com/blogspot/debukkitsblog/Crypt/CryptedObject.java
@@ -0,0 +1,22 @@
+package com.blogspot.debukkitsblog.Crypt;
+
+import java.io.Serializable;
+
+public class CryptedObject implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8303052061596400734L;
+	
+	private byte[] bytes;
+	
+	public CryptedObject(byte[] bytes) {
+		this.bytes = bytes;
+	}
+	
+	public byte[] getBytes(){
+		return bytes;
+	}
+
+}
